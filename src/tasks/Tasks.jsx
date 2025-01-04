655853b4917ba32789c81cde413bbe42d8ca3b42
Piyush Goyal <piyush2022ug@gmail.com>
2025-01-04 07:00:39 +0530

    RX3_HW3 Done

--- src/tasks/Tasks.jsx
+++ src/tasks/Tasks.jsx
@@ -1,24 +1,41 @@
 import { useSelector, useDispatch } from "react-redux";
-import {statusBtnPress} from './taskSlice';
+import { statusBtnPress, fetchTasks } from "./taskSlice";
+import { useEffect } from "react";
 
 export default function Tasks() {
   const dispatch = useDispatch();
-  const tasks = useSelector((state) => {
-    console.log(state.tasks);
-    return state.tasks;
-  });
+  const tasks = useSelector((state) => state.tasks.tasks);
+  const status = useSelector((state) => state.tasks.status);
+  const error = useSelector((state) => state.tasks.error);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
 
   return (
     <>
       <h1>My Task List</h1>
-      {tasks.tasks.map((task) => (
+      {status === "loading" && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {tasks?.map((task) => (
         <div key={task.date}>
           <h2>{task.date}</h2>
           <ul>
-            {task.todos.map((todo, todoIndex) => (
-              <li>
-                {todo.toDo}
-                <button onClick={() => dispatch(statusBtnPress({date: task.date, todoIndex}))}>{todo.status}</button>
+            {task.tasks.map((todo) => (
+              <li key={todo.taskId}>
+                {todo.task}
+                <button
+                  onClick={() =>
+                    dispatch(
+                      statusBtnPress({
+                        date: task.date,
+                        taskId: todo.taskId,
+                      })
+                    )
+                  }
+                >
+                  {todo.taskStatus}
+                </button>
               </li>
             ))}
           </ul>
--- src/tasks/taskSlice.js
+++ src/tasks/taskSlice.js
@@ -1,53 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
+  const response = await axios.get(
+    "https://task-list-hw-server-student-neog-ca.replit.app/tasks"
+  );
+
+  return response.data;
+});
 
 export const taskSlice = createSlice({
   name: "tasks",
   initialState: {
-    tasks: [
-      {
-        date: "15/07/2024",
-        todos: [
-          {
-            toDo: "Get Groceries from the market.",
-            status: "Pending",
-          },
-          {
-            toDo: "Go to Gym.",
-            status: "Completed",
-          },
-          {
-            toDo: "Water the plants.",
-            status: "Completed",
-          },
-        ],
-      },
-      {
-        date: "16/07/2024",
-        todos: [
-          {
-            toDo: "Go to the park.",
-            status: "Completed",
-          },
-          {
-            toDo: "Get my room cleaned.",
-            status: "Pending",
-          },
-        ],
-      },
-    ],
+    tasks: [],
+    status: "idle",
+    error: null,
   },
   reducers: {
     statusBtnPress: (state, action) => {
-      const { date, todoIndex } = action.payload;
+      const { date, taskId } = action.payload;
       const task = state.tasks.find((task) => task.date === date);
       if (task) {
-        task.todos[todoIndex].status =
-          task.todos[todoIndex].status === "Pending" ? "Completed" : "Pending";
+        const todo = task.tasks.find((t) => t.taskId === taskId);
+        if (todo) {
+          todo.taskStatus =
+            todo.taskStatus === "Pending" ? "Completed" : "Pending";
+        }
       }
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchTasks.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchTasks.fulfilled, (state, action) => {
+      state.status = "success";
+      state.tasks = action.payload.tasks;
+    });
+    builder.addCase(fetchTasks.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.payload.message;
+    });
+  },
 });
 
-export const {statusBtnPress} = taskSlice.actions
+export const { statusBtnPress } = taskSlice.actions;
 
 export default taskSlice.reducer;
